429b584d982ce5e02ae1ca8421d50b2426a149fd
Cauê catone silva <cauecatonesilva@gmail.com>
2023-08-30 15:22:16 -0400

    responsividade retomada

--- main.js
+++ main.js
@@ -1,53 +1,67 @@
   import html2canvas from '/html2canvas/dist/html2canvas.esm.js';
 
-
-
   const input = document.querySelector('#username');
   const inputsemestre = document.querySelector('#semestre');
+  const inputra = document.querySelector('#ra');
   const button = document.querySelector('#gerarticket');
-
 
   const nameElement = document.querySelector('#name');
   const semestreElement = document.querySelector('#semestreinput');
+  const raElement = document.querySelector('#rainput');
   const photoElement = document.querySelector('#photo');
   const errorElement = document.querySelector('#error');
-
 
   const buttonDownload = document.querySelector('#Download');
 
   let data;
 
   button.addEventListener('click', async () => {
-    console.log('chegou');
+
     const username = input.value;
     const response = await fetch(`https://api.github.com/users/${username}`);
     data = await response.json();
     const name = data.name;
     const semestre = inputsemestre.value;
+    const ra = inputra.value;
     const photo = data.avatar_url;
 
-  console.log(data)
+
+    if (data !== undefined && data.message === 'Not Found' || input.value == '') {
+
+      const [name, setName] = useState("");
+      const [ra, setRa] = useState("");
+      const [converter, setConverter] = useState("");
 
 
-    if (data !== undefined && data.message === 'Not Found' && input.value == '') {
-      errorElement.style.display = 'block';
       nameElement.style.display = 'block';
       photoElement.style.display = 'block';
+      raElement.style.display = 'block';
+      buttonDownload.style.display = 'block';
+
+      nameElement.innerHTML = username;
+      semestreElement.innerHTML = semestre;
+      raElement.innerHTML = ra;
+      photoElement.setAttribute('src', photodefault);
+
     } else {
-      console.log('chegou2');
+
       errorElement.style.display = 'none';
       nameElement.style.display = 'block';
       photoElement.style.display = 'block';
+      raElement.style.display = 'block';
       buttonDownload.style.display = 'block';
+
       nameElement.innerHTML = name;
       semestreElement.innerHTML = semestre;
+      raElement.innerHTML = ra;
       photoElement.setAttribute('src', photo);
+
     }
+
   });
 
 
   buttonDownload.addEventListener('click', async () => {
-    console.log('chegou3');
   
     // Carregar a imagem
     const image = new Image();
@@ -76,8 +90,7 @@
   
       // Remova o link após o clique ser simulado
       document.body.removeChild(downloadLink);
-      
-      console.log('chegou4');
+
     });
   });
 
